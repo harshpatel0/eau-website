2d222402b9648bcc798b933d63feff295a1ff746
Harsh Patel <58038014+harshpatel0@users.noreply.github.com>
2024-09-23 22:09:21 +0300

    Added All Articles Pane

---
+++ src/AllArticles.jsx
@@ -0,0 +1,42 @@
+function AllArticles() {
+  return (
+    <div className="all-articles">
+      <section className="articles-list">
+        <h1>For the time being, it's music</h1>
+        <ul>
+          <li className="articles-list-item">
+            <h2>R U Mine?</h2>
+            <p>Arctic Monkeys</p>
+          </li>
+          <li className="articles-list-item">
+            <h2>Dancing in the Flames</h2>
+            <p>The Weeknd</p>
+          </li>
+          <li className="articles-list-item">
+            <h2>About You</h2>
+            <p>The 1975</p>
+          </li>
+          <li className="articles-list-item">
+            <h2>Mad Now</h2>
+            <p>Upstrz</p>
+          </li>
+          <li className="articles-list-item">
+            <h2>sdp interlude</h2>
+            <p>Travis Scott</p>
+          </li>
+          <li className="articles-list-item">
+            <h2>Runaway</h2>
+            <p>Kanye West</p>
+          </li>
+          <li className="articles-list-item">
+            <h2>Michelle</h2>
+            <p>Sir Chloe</p>
+          </li>
+        </ul>
+      </section>
+      <section></section>
+    </div>
+  );
+}
+
+export default AllArticles;
