3d920ec6dd9e48f50e4caf9da94f58cc8d63d34e
Harsh Patel <58038014+harshpatel0@users.noreply.github.com>
2024-09-23 22:09:13 +0300

    Done alot regarding previous commits

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,47 @@
-import "./App.css";
+import React, { useState } from "react";
+import { useInView } from "react-intersection-observer";
+
 import Navbar from "./Navbar.jsx";
 import HeroSection from "./HeroSection.jsx";
-import React, { useState } from "react";
+import Heading from "./Heading.jsx";
+import AllArticles from "./AllArticles.jsx";
+import "./App.css";
+
 import SunkenShipBackgroundImage from "./assets/images/SunkenSwamp.jpg";
+import Crashed from "./assets/images/Crashed.jpg";
+import NewYork from "./assets/images/NewYork.jpg";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
+
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+  });
+  // This state variable is used to tell if the hero sections are visible, if all hero sections are not visible anymore, then we will set it to false
   return (
     <>
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
-      <div className="hero-content">
-        <HeroSection
-          title="Plants and their answer to life"
-          author="Harsh Patel"
-          image={SunkenShipBackgroundImage}
-        />
-        <HeroSection
-          title="Plane Crash"
-          author="John from Crashed!"
-          image={SunkenShipBackgroundImage}
-        />
-        <HeroSection
-          title="Waves"
-          author="Mary from Snapped!"
-          image={SunkenShipBackgroundImage}
-        />
-      </div>
+      <Heading headerText={inView ? "Featured Articles" : "All Articles"} />
+      <main className="flex-container">
+        <div ref={ref} className="hero-content">
+          <HeroSection
+            title="Plants and their answer to life"
+            author="Harsh Patel"
+            image={SunkenShipBackgroundImage}
+          />
+          <HeroSection
+            title="Plane Crash"
+            author="John from Crashed!"
+            image={Crashed}
+          />
+          <HeroSection
+            title="Metropolitan Jungle"
+            author="Alex from Visit New York"
+            image={NewYork}
+          />
+        </div>
+        <AllArticles />
+      </main>
     </>
   );
 }
