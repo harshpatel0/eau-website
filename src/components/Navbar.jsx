f43802c376fa5777bddd94da2d412f9cff6486d7
Harsh Patel <58038014+harshpatel0@users.noreply.github.com>
2024-09-28 20:52:43 +0300

    Using contexts instead of props

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,10 @@ import {
   Sun,
   MoonStars,
 } from "@phosphor-icons/react";
+import { useContext } from "react";
+import { DarkModeContext } from "../contexts/DarkModeContext";
 function Navbar(props) {
+  const [darkMode, toggleTheme] = useContext(DarkModeContext);
   return (
     <nav className="navbar">
       <IconContext.Provider
@@ -18,13 +21,10 @@ function Navbar(props) {
       >
         <House alt="Home" className="icons" />
         <Article alt="All Articles" className="icons" />
-        {props.darkMode ? (
-          <Sun onClick={props.toggleTheme} className="icons theme-selector" />
+        {darkMode ? (
+          <Sun onClick={toggleTheme} className="icons theme-selector" />
         ) : (
-          <MoonStars
-            onClick={props.toggleTheme}
-            className="icons theme-selector"
-          />
+          <MoonStars onClick={toggleTheme} className="icons theme-selector" />
         )}
       </IconContext.Provider>
     </nav>
