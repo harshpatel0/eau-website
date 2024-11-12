c8d1032fd1dddf1cc8e07ec4ba03a28df2361ead
Harsh Patel <58038014+harshpatel0@users.noreply.github.com>
2024-11-12 21:01:41 +0300

    Removing Loading Screen Test Page

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,20 @@ import UnionDispatch from "./pages/UnionDispatch/UnionDispatch.jsx";
 import Article from "./pages/UnionDispatch/Article/Article.jsx";
 import TestPage from "./pages/UnionDispatch/TestPage.jsx";
 
-// Test Pages
-import LoadingScreenTest from "./pages/LoadingScreen.jsx";
-
 import Error from "./pages/Error/Error.jsx";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="*" element={<Error />} />
         <Route path="/" element={<Home />} />
         <Route path="/uniondispatch" element={<UnionDispatch />} />
         <Route path="/uniondispatch/testpage" element={<TestPage />} />
         <Route
-          path="/testpages/loadingscreen"
-          element={<LoadingScreenTest />}
-        />
-
-        <Route
           path="/uniondispatch/articles/:articleId"
           element={<Article />}
         />
+        <Route path="*" element={<Error />} />
       </Routes>
     </>
   );
