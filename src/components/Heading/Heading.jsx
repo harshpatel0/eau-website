ec22a4b0e9e5a0b9b1fd190a42152e789d094ce4
Harsh Patel <58038014+harshpatel0@users.noreply.github.com>
2024-11-13 22:38:34 +0300

    Fixed an issue where the icons and text may not be visible in certain modes behind certain colours, and arranged imports

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,9 +1,9 @@
-import { useState, useContext } from "react";
-import PropTypes from "prop-types";
+import "./Heading.css";
+
+import { useContext, useState } from "react";
 
 import { DarkModeContext } from "../../contexts/DarkModeContext.jsx";
-
-import "./Heading.css";
+import PropTypes from "prop-types";
 
 // Props Taken
 //    HeaderText: string
@@ -25,12 +25,20 @@ function Heading(props) {
         : darkMode
         ? "2px 4px 10px 1px whitesmoke" // White border in dark mode
         : "2px 4px 10px 1px black", // Black border in light mode
+    backdropFilter: darkMode
+      ? "blur(100px) brightness(70%)"
+      : "blur(100px) brightness(90%)",
+    WebkitBackdropFilter: darkMode
+      ? "blur(100px) brightness(70%)"
+      : "blur(100px) brightness(90%)",
   };
 
   return (
     <div style={styles} className="location-heading">
-      <h1>{props.headerText}</h1>
-      <p style={{ fontSize: "1.1rem" }}>{props.subText}</p>
+      <h1 className="heading-text">{props.headerText}</h1>
+      <p className="heading-subtext" style={{ fontSize: "1.1rem" }}>
+        {props.subText}
+      </p>
     </div>
   );
 }
