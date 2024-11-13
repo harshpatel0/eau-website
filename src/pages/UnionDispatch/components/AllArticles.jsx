28fc26b79facd19e20fe53a4dd76f29069ff770a
Harsh Patel <58038014+harshpatel0@users.noreply.github.com>
2024-11-13 22:40:59 +0300

    Added private article functionality (they won't be shown in All Articles or Featured); Rearranged imports

--- src/pages/UnionDispatch/components/AllArticles.jsx
+++ src/pages/UnionDispatch/components/AllArticles.jsx
@@ -8,6 +8,7 @@ import { apiBaseUrl } from "../../../veryglobalvars";
 
 function AllArticles(props) {
   const [articles, setArticles] = useState([]);
+  const [isArticlePublic, setIsArticlePublic] = useState(true);
   let styles = {
     backgroundColor: props.darkMode ? "#1f1f1fff" : "whitesmoke",
     color: props.darkMode ? "whitesmoke" : "#1f1f1fff",
@@ -18,6 +19,7 @@ function AllArticles(props) {
       .get(apiBaseUrl + "/articles/all")
       .then(function (response) {
         setArticles(response.data);
+        console.log(articles);
       })
       .catch(function (error) {
         console.log(error);
@@ -28,14 +30,19 @@ function AllArticles(props) {
       <div style={styles} className="all-articles">
         <section className="articles-list">
           <ul>
-            {articles.map((article) => (
-              <li className="articles-list-item" key={article.article_id}>
-                <Link to={`/uniondispatch/articles/${article.article_id}`}>
-                  <h2>{article.title}</h2>
-                </Link>
-                <p>{article.author_name}</p>
-              </li>
-            ))}
+            {articles.map((article) =>
+              article.public == "1" ? (
+                <li className="articles-list-item" key={article.article_id}>
+                  <Link to={`/uniondispatch/articles/${article.article_id}`}>
+                    <h2>{article.title}</h2>
+                  </Link>
+                  <p>{article.author_name}</p>
+                </li>
+              ) : (
+                // <li>Article Hidden</li>
+                <></>
+              )
+            )}
           </ul>
         </section>
         <section></section>
