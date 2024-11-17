9c8791c6d8fc38657f4cdb6821ba61652e53036b
Harsh Patel <58038014+harshpatel0@users.noreply.github.com>
2024-11-17 17:00:11 +0300

    Reverting global vars to localhost

--- src/veryglobalvars.js
+++ src/veryglobalvars.js
@@ -1,5 +1,5 @@
-// export const apiBaseUrl = "http://localhost:5000";
-// export const websiteUrl = "http://localhost:5173";
+export const apiBaseUrl = "http://localhost:5000";
+export const websiteUrl = "http://localhost:5173";
 
-export const apiBaseUrl = "http://192.168.0.101:5000";
-export const websiteUrl = "http://192.168.0.101:5173";
+// export const apiBaseUrl = "http://192.168.0.101:5000";
+// export const websiteUrl = "http://192.168.0.101:5173";
