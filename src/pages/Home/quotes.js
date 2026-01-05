3831e8f2f2dfbe0826474e43bf87a54af0d16263
Harsh Patel <58038014+harshpatel0@users.noreply.github.com>
2026-01-05 12:34:22 +0300

    Remove multiple quotes from quotes.js
    Removed several quotes and captions from the quotes array.

--- src/pages/Home/quotes.js
+++ src/pages/Home/quotes.js
@@ -1,35 +1,5 @@
 export const quotes = [
   {
-    quote: "Make a choice, oxygen or WiFi",
-    caption: "Chebeni Musundi, President",
-  },
-  {
-    quote: "Site made with cello tape and cardboard",
-    caption: "Harsh Patel, Vice President",
-  },
-  {
-    quote: "Upholding Security 👮🚓",
-    caption: "Jeremy Mwangi, CSO",
-  },
-  {
-    quote:
-      "Even if you're not always ready for the day, it can not always be night",
-    caption: "Chebeni Musundi, President",
-  },
-  {
-    quote: "Its never that deep.",
-    caption: "Keval Patel, Coordinator",
-  },
-  {
-    quote:
-      "When life gives you oranges you eat apple because they look as good as the forbidden fruit",
-    caption: "Amrit Metha, Head of Film / Media Director",
-  },
-  {
-    quote: `You're too strong how did you lose and I replied with\n"I can fight 100 men looking at her but I can't fight the one she's looking at."`,
-    caption: "Amrit Metha, Head of Film / Media Director",
-  },
-  {
     quote: `What do you call an aligator in a vest...`,
     caption: "...An Investigator 🥁🥁🥁",
   },
